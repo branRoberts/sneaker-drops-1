91765a7404db31fd59a99e93abbaedb7444f89ba
branRoberts <brandonkrob12@gmail.com>
2026-06-08 13:49:25 -0400

    Second Commit I followed the instructions and added a new class and added the annotation to the class

--- src/test/java/com/pluralsight/sneakerdrops/SneakerDrops1ApplicationTests.java
+++ src/test/java/com/pluralsight/sneakerdrops/SneakerDrops1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SneakerDrops1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
